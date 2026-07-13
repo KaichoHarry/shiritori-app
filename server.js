dfcdbb5ca3d1953ee6c0eb7ff0414c00c18d401b
KaichoHarry <nanko.haruka@gmail.com>
2026-07-13 16:58:36 +0900

    サーバー変数を確認

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+// server.js
+// アクセス数を保持する変数をグローバル領域に定義
+let count = 0;
+
 // localhostにDenoのHTTPサーバーを展開
-Deno.serve((_req) => new Response("Hello Deno!"));
+Deno.serve((_req) => {
+    count++;
+    return new Response(`Hello World! ${count}`);
+});
