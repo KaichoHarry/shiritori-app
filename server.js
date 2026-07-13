4b979ecd1656756a8d19ce3cd83eccfcd70a93a0
KaichoHarry <nanko.haruka@gmail.com>
2026-07-13 17:04:04 +0900

    index.htmlを用いてhtmlを表示させる

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 
 // localhostにDenoのHTTPサーバーを展開
-Deno.serve((_req) => {
+Deno.serve(async (_req) => {
+    const htmlText = await Deno.readTextFile("./public/index.html");
+
     return new Response(
         // Responseの第一引数にレスポンスのbodyを設置
-        "<h1>H1見出しです</h1>",
+        htmlText,
         // Responseの第二引数にヘッダ情報等の付加情報を設置
         {
             // レスポンスにヘッダ情報を付加
